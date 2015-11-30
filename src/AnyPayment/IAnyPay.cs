7ff8e902108f402bffdbe987b26db3e9aabaa514
莴笋Ho <gzhehai@foxmail.com>
2015-11-30 19:24:48 +0800

    init project

---
+++ src/AnyPayment/IAnyPay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyPayment
+{
+    public interface IAnyPay
+    {
+        Dictionary<string, object> ReqParameters { get; }
+    }
+}
