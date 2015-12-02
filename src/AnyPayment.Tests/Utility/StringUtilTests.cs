3c782a621ac2349068b984c5d7d90d0287ce5c12
莴笋Ho <gzhehai@foxmail.com>
2015-12-03 00:20:01 +0800

    add Nuget tool

---
+++ src/AnyPayment.Tests/Utility/StringUtilTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnyPayment.Tests.Utility
+{
+    [TestClass]
+    public class StringUtilTests
+    {
+        [TestMethod]
+        public void EncodeTests()
+        {
+        }
+    }
+}
